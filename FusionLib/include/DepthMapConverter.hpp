cdca479fb360b3b14d234624596b5e8849a9cdea
bockph <philipp.bock@hotmail.de>
2019-06-28 16:28:26 +0200

    Implemented computeNormals in DepthMapConverter

--- FusionLib/include/DepthMapConverter.hpp
+++ FusionLib/include/DepthMapConverter.hpp
@@ -12,7 +12,13 @@
 class DepthMapConverter{
 public:
     DepthMapConverter(std::shared_ptr<std::vector<Point2D>> imageData,int imageWidth,int imageHeight, Eigen::Matrix3f intrinsics, Eigen::Matrix4f extrinsics);
+
     void ImageToWorld();
+    /*
+     * This Method assumes, that _camera Vertex contains the vertices in row-major order,
+     * Therefore parallelization isnt possible as the order might be changed
+     */
+    void computeNormals();
 
 
 
@@ -21,6 +27,8 @@ private:
     std::shared_ptr<std::vector<Point2D>> _imageData;
     std::shared_ptr<std::vector<Vector3f>> _cameraVertex;
     std::shared_ptr<std::vector<Vector4f>> _globalVertex;
+    std::shared_ptr<std::vector<Vector3f>> _cameraNormals;
+    std::shared_ptr<std::vector<Vector4f>> _globalNormals;
     Eigen::Matrix3f _intrinsics;
     Eigen::Matrix4f _extrinsics;
     int _width,_height;
@@ -35,5 +43,9 @@ public:
     const std::shared_ptr<std::vector<Vector3f>> &getCameraVertex() const;
 
     const std::shared_ptr<std::vector<Vector4f>> &getGlobalVertex() const;
+
+    const std::shared_ptr<std::vector<Vector3f>> &getCameraNormal() const;
+
+    const std::shared_ptr<std::vector<Vector4f>> &getGlobalNormals() const;
 
 };
