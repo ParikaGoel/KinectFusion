4bfce3374fc36e7bfb1f30ebd50abff24c7c8d3e
bockph <philipp.bock@hotmail.de>
2019-06-28 16:28:26 +0200

    Implemented computeNormals in DepthMapConverter

--- FusionLib/src/DepthMapConverter.cpp
+++ FusionLib/src/DepthMapConverter.cpp
@@ -1,5 +1,9 @@
-#include "DepthMapConverter.hpp"
 
+//
+// Created by Philipp Bock on 28.06.19.
+//
+
+#include "DepthMapConverter.hpp"
 
 
 DepthMapConverter::DepthMapConverter(std::shared_ptr<std::vector<Point2D>> imageData,int imageWidth,int imageHeight, Eigen::Matrix3f intrinsics,
@@ -12,8 +16,11 @@ DepthMapConverter::DepthMapConverter(std::shared_ptr<std::vector<Point2D>> image
 
     _globalVertex = std::make_shared<std::vector<Vector4f>>(imageData->size());
     _cameraVertex = std::make_shared<std::vector<Vector3f>>(imageData->size());
+    _globalNormals = std::make_shared<std::vector<Vector4f>>(imageData->size());
+    _cameraNormals = std::make_shared<std::vector<Vector3f>>(imageData->size());
 
     ImageToWorld();
+    computeNormals();
 
 }
 
@@ -38,11 +45,49 @@ void DepthMapConverter::ImageToWorld() {
     }
 }
 
+void DepthMapConverter::computeNormals(){
+    auto trajectoryInv = _extrinsics.inverse();
+    Vector4f globalNormal(0,0,0,1);
+    for(int y=0;y<_height;y++){
+        for(int x =0;x<_width;x++){
+            /*
+             *
+             * Debugging Variables:
+             *
+            int xx = _imageData->at(y*_width+x)._position.x();
+            int yy = _imageData->at(y*_width+x)._position.y();
+
+             */
+
+            if(_imageData->at(y*_width+x)._position.x()!=x ||_imageData->at(y*_width+x)._position.y()!=y)
+                throw std::invalid_argument(" The order of _imageData is not correct (e.g. not Row-major or parrallelization applied) ");
+
+
+            Vector3f v1=_cameraVertex->at(y*_width+x+1)-_cameraVertex->at(y*_width+x);
+            Vector3f v2=_cameraVertex->at((y+1)*_width+x)-_cameraVertex->at(y*_width+x);
+            auto normal = v1.cross(v2);
+            normal.normalize();
+            _cameraNormals->push_back(normal);
+            globalNormal.block(0,0,3,1) = normal;
+            globalNormal = trajectoryInv * globalNormal;
+            _globalNormals->push_back(globalNormal);
+        }
+    }
+}
+
 const std::shared_ptr<std::vector<Vector3f>> &DepthMapConverter::getCameraVertex() const {
     return _cameraVertex;
 }
 
 const std::shared_ptr<std::vector<Vector4f>> &DepthMapConverter::getGlobalVertex() const {
     return _globalVertex;
+}
+
+const std::shared_ptr<std::vector<Vector3f>> &DepthMapConverter::getCameraNormal() const {
+    return _cameraNormals;
+}
+
+const std::shared_ptr<std::vector<Vector4f>> &DepthMapConverter::getGlobalNormals() const {
+    return _globalNormals;
 }
 
