9273487adf17b7eff425b8503f3e01da9f9690d9
ga96tul <Freemander10>
2019-06-29 09:50:47 +0200

    Reformat Code

--- FusionLib/include/DepthMapConverter.hpp
+++ FusionLib/include/DepthMapConverter.hpp
@@ -9,46 +9,51 @@
 #include <Utils.hpp>
 #include <EigenHelper.hpp>
 
-class DepthMapConverter{
+class DepthMapConverter {
 public:
-    DepthMapConverter(std::shared_ptr<std::vector<Point2D>> imageData,int imageWidth,int imageHeight, Eigen::Matrix3d
-    intrinsics, Eigen::Matrix4d extrinsics);
+	DepthMapConverter(std::shared_ptr<std::vector<Point2D>> imageData, int imageWidth, int imageHeight, Eigen::Matrix3d
+	intrinsics, Eigen::Matrix4d extrinsics);
 
-    void ImageToWorld();
-    /*
-     * This Method assumes, that _camera Vertex contains the vertices in row-major order,
-     * Therefore parallelization isnt possible as the order might be changed
-     */
-    void computeNormals();
+	void ImageToWorld();
 
-
+	/*
+	 * This Method assumes, that _camera Vertex contains the vertices in row-major order,
+	 * Therefore parallelization isnt possible as the order might be changed
+	 */
+	void computeNormals();
 
 
 private:
-    std::shared_ptr<std::vector<Point2D>> _imageData;
-    std::shared_ptr<std::vector<Vector3d>> _cameraVertex;
-    std::shared_ptr<std::vector<Vector4d>> _globalVertex;
-    std::shared_ptr<std::vector<Vector3d>> _cameraNormals;
-    std::shared_ptr<std::vector<Vector4d>> _globalNormals;
-    Eigen::Matrix3d _intrinsics;
-    Eigen::Matrix4d _extrinsics;
-    int _width,_height;
+	std::shared_ptr<std::vector<Point2D>> _imageData;
+	std::shared_ptr<std::vector<Vector3d>> _cameraVertex;
+	std::shared_ptr<std::vector<Vector4d>> _globalVertex;
+	std::shared_ptr<std::vector<Vector3d>> _cameraNormals;
+	std::shared_ptr<std::vector<Vector4d>> _globalNormals;
+	Eigen::Matrix3d _intrinsics;
+	Eigen::Matrix4d _extrinsics;
+	int _width, _height;
 
-    /*
-     *
-     * Getters & Setters
-     *
-     */
+	/*
+	 *
+	 * Getters & Setters
+	 *
+	 */
 public:
 
-    const std::shared_ptr<std::vector<Vector3d>> &getCameraVertexPtr() const;
-    const std::shared_ptr<std::vector<Vector4d>> &getGlobalVertexPtr() const;
-    const std::shared_ptr<std::vector<Vector3d>> &getCameraNormalPtr() const;
-    const std::shared_ptr<std::vector<Vector4d>> &getGlobalNormalsPtr() const;
+	const std::shared_ptr<std::vector<Vector3d>> &getCameraVertexPtr() const;
+
+	const std::shared_ptr<std::vector<Vector4d>> &getGlobalVertexPtr() const;
+
+	const std::shared_ptr<std::vector<Vector3d>> &getCameraNormalPtr() const;
+
+	const std::shared_ptr<std::vector<Vector4d>> &getGlobalNormalsPtr() const;
 
 	const std::vector<Vector3d> &getCameraVertex() const;
+
 	const std::vector<Vector4d> &getGlobalVertex() const;
+
 	const std::vector<Vector3d> &getCameraNormal() const;
+
 	const std::vector<Vector4d> &getGlobalNormals() const;
 
 };
--- FusionLib/src/DepthMapConverter.cpp
+++ FusionLib/src/DepthMapConverter.cpp
@@ -6,74 +6,76 @@
 #include "DepthMapConverter.hpp"
 
 
-DepthMapConverter::DepthMapConverter(std::shared_ptr<std::vector<Point2D>> imageData,int imageWidth,int imageHeight,
-		Eigen::Matrix3d intrinsics,
-                                     Eigen::Matrix4d extrinsics):
-                                     _imageData(imageData),
-                                     _intrinsics(intrinsics),
-                                     _extrinsics(extrinsics),
-                                     _width(imageWidth),
-                                     _height(imageHeight){
+DepthMapConverter::DepthMapConverter(std::shared_ptr<std::vector<Point2D>> imageData, int imageWidth, int imageHeight,
+									 Eigen::Matrix3d intrinsics, Eigen::Matrix4d extrinsics)
+		:
+		_imageData(imageData)
+		, _intrinsics(intrinsics)
+		, _extrinsics(extrinsics)
+		, _width(imageWidth)
+		, _height(imageHeight) {
 
-    _globalVertex = std::make_shared<std::vector<Vector4d>>(imageData->size());
-    _cameraVertex = std::make_shared<std::vector<Vector3d>>(imageData->size());
-    _globalNormals = std::make_shared<std::vector<Vector4d>>(imageData->size());
-    _cameraNormals = std::make_shared<std::vector<Vector3d>>(imageData->size());
+	_globalVertex = std::make_shared<std::vector<Vector4d>>(imageData->size());
+	_cameraVertex = std::make_shared<std::vector<Vector3d>>(imageData->size());
+	_globalNormals = std::make_shared<std::vector<Vector4d>>(imageData->size());
+	_cameraNormals = std::make_shared<std::vector<Vector3d>>(imageData->size());
 
-    ImageToWorld();
-    computeNormals();
+	ImageToWorld();
+	computeNormals();
 
 }
 
 void DepthMapConverter::ImageToWorld() {
-    auto intrinsicsInverse = _intrinsics.inverse();
-    auto trajectoryInv = _extrinsics.inverse();
-    for(Point2D vertex :*_imageData){
-        Vector4d worldVertex(0,0,0,1);
-        Vector3d cameraVertex;
+	auto intrinsicsInverse = _intrinsics.inverse();
+	auto trajectoryInv = _extrinsics.inverse();
+	for (Point2D vertex :*_imageData) {
+		Vector4d worldVertex(0, 0, 0, 1);
+		Vector3d cameraVertex;
 
-        if(vertex._data == MINF){
-            worldVertex = Vector4d(MINF,MINF,MINF,MINF);
-        }else{
-            //calc depthvalue*K⁻1*[u,1]
-            Vector3d cameraVertex =vertex._data*intrinsicsInverse*vertex._position;
-            worldVertex.block(0,0,3,1) = cameraVertex;
-            worldVertex = trajectoryInv * worldVertex;
-        }
-        _cameraVertex->push_back(cameraVertex);
-        _globalVertex->push_back(worldVertex);
+		if (vertex._data == MINF) {
+			worldVertex = Vector4d(MINF, MINF, MINF, MINF);
+		} else {
+			//calc depthvalue*K⁻1*[u,1]
+			Vector3d cameraVertex = vertex._data * intrinsicsInverse * vertex._position;
+			worldVertex.block(0, 0, 3, 1) = cameraVertex;
+			worldVertex = trajectoryInv * worldVertex;
+		}
+		_cameraVertex->push_back(cameraVertex);
+		_globalVertex->push_back(worldVertex);
 
-    }
+	}
 }
 
-void DepthMapConverter::computeNormals(){
-    auto trajectoryInv = _extrinsics.inverse();
-    Vector4d globalNormal(0,0,0,1);
-    for(int y=0;y<_height;y++){
-        for(int x =0;x<_width;x++){
-            /*
-             *
-             * Debugging Variables:
-             *
-            int xx = _imageData->at(y*_width+x)._position.x();
-            int yy = _imageData->at(y*_width+x)._position.y();
+void DepthMapConverter::computeNormals() {
+	auto trajectoryInv = _extrinsics.inverse();
+	Vector4d globalNormal(0, 0, 0, 1);
+	for (int y = 0; y < _height; y++) {
+		for (int x = 0; x < _width; x++) {
+			/*
+			 *
+			 * Debugging Variables:
+			 *
+			int xx = _imageData->at(y*_width+x)._position.x();
+			int yy = _imageData->at(y*_width+x)._position.y();
 
-             */
+			 */
 
-            if(_imageData->at(y*_width+x)._position.x()!=x ||_imageData->at(y*_width+x)._position.y()!=y)
-                throw std::invalid_argument(" The order of _imageData is not correct (e.g. not Row-major or parrallelization applied) ");
+			if (_imageData->at(y * _width + x)._position.x() != x ||
+				_imageData->at(y * _width + x)._position.y() != y) {
+				throw std::invalid_argument(" The order of _imageData is not correct (e.g. not Row-major or parrallelization applied) ");
+			}
 
 
-            Vector3d v1=_cameraVertex->at(y*_width+x+1)-_cameraVertex->at(y*_width+x);
-            Vector3d v2=_cameraVertex->at((y+1)*_width+x)-_cameraVertex->at(y*_width+x);
-            auto normal = v1.cross(v2);
-            normal.normalize();
-            _cameraNormals->push_back(normal);
-            globalNormal.block(0,0,3,1) = normal;
-            globalNormal = trajectoryInv * globalNormal;
-            _globalNormals->push_back(globalNormal);
-        }
-    }
+			Vector3d v1 = _cameraVertex->at(y * _width + x + 1) - _cameraVertex->at(y * _width + x);
+			Vector3d v2 = _cameraVertex->at((y + 1) * _width + x) - _cameraVertex->at(y * _width + x);
+			auto normal = v1.cross(v2);
+			normal.normalize();
+			_cameraNormals->push_back(normal);
+			globalNormal.block(0, 0, 3, 1) = normal;
+			globalNormal = trajectoryInv * globalNormal;
+			_globalNormals->push_back(globalNormal);
+		}
+	}
 }
 
 const std::shared_ptr<std::vector<Vector3d>> &DepthMapConverter::getCameraVertexPtr() const {
