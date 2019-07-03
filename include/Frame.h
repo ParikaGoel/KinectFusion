152f575a26e3def3b2043ec6b43ecb4e12f12125
Frank <frankz.lu@gmail.com>
2019-07-03 18:46:11 +0200

    shizzle

--- include/Frame.h
+++ include/Frame.h
@@ -5,203 +5,68 @@
 
 #include <limits>
 
+#include <vector>
+
 #ifndef MINF
 #define MINF -std::numeric_limits<double>::infinity()
 #endif
 
+
+
+
 class Frame {
 public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-    Frame() {}
 
-    Frame(float* depthMap, const Eigen::Matrix3d& depthIntrinsics, const Eigen::Matrix4d& depthExtrinsics, const unsigned width, const unsigned height, unsigned downsampleFactor = 1, float maxDistance = 0.1f) {
-		// Get depth intrinsics.
-		float fovX = depthIntrinsics(0, 0);
-		float fovY = depthIntrinsics(1, 1);
-		float cX = depthIntrinsics(0, 2);
-		float cY = depthIntrinsics(1, 2);
-        intrinsic_matrix = depthIntrinsics;
+    Frame(std::vector<double> depthMap, const Eigen::Matrix3d& depthIntrinsics,
+            const unsigned int width, const unsigned int height, int downsampleFactor = 1, double maxDistance = 0.1);
 
-		// Compute inverse depth extrinsics.
-        Eigen::Matrix4d depthExtrinsicsInv = depthExtrinsics.inverse();
-        Eigen::Matrix3d rotationInv = depthExtrinsicsInv.block(0, 0, 3, 3);
-        Eigen::Vector3d translationInv = depthExtrinsicsInv.block(0, 3, 3, 1);
+    Frame(std::vector<double> depthMap, const Eigen::Matrix3d& depthIntrinsics, const Eigen::Matrix4d& depthExtrinsics,
+            const unsigned int width, const unsigned int height);
 
-		// Back-project the pixel depths into the camera space.
-		std::vector<Eigen::Vector3d> pointsTmp(width * height);
+    Frame(std::vector<double> depthMap, const Eigen::Matrix3d& depthIntrinsics, const Eigen::Matrix4d& depthExtrinsics,
+            const unsigned width, const unsigned height, unsigned downsampleFactor = 1, float maxDistance = 0.1f);
 
-		// For every pixel row.
-		#pragma omp parallel for
-		for (int v = 0; v < height; ++v) {
-			// For every pixel in a row.
-			for (int u = 0; u < width; ++u) {
-				unsigned int idx = v*width + u; // linearized index
-				double depth = depthMap[idx];
-				if (depth == MINF) {
-					pointsTmp[idx] = Eigen::Vector3d(MINF, MINF, MINF);
-				}
-				else {
-					// Back-projection to camera space.
-					pointsTmp[idx] = rotationInv * Eigen::Vector3d((u - cX) / fovX * depth, (v - cY) / fovY * depth, depth) + translationInv;
-				}
-			}
-		}
+	void computeNormals(double maxDistance=0.1);
 
-		// We need to compute derivatives and then the normalized normal vector (for valid pixels).
-		std::vector<Eigen::Vector3d> normalsTmp(width * height);
+	bool readFromFile(const std::string& filename);
 
-		#pragma omp parallel for
-		for (int v = 1; v < height - 1; ++v) {
-			for (int u = 1; u < width - 1; ++u) {
-				unsigned int idx = v*width + u; // linearized index
+	std::vector<Eigen::Vector3d>& getPoints();
 
-				const double du = depthMap[idx + 1] - depthMap[idx - 1];
-				const double dv = depthMap[idx + width] - depthMap[idx - width];
-				if (!std::isfinite(du) || !std::isfinite(dv) || abs(du) > maxDistance || abs(dv) > maxDistance) {
-					normalsTmp[idx] = Eigen::Vector3d(MINF, MINF, MINF);
-					continue;
-				}
+	const std::vector<Eigen::Vector3d>& getPoints() const;
 
-				// TODO: Compute the normals using the cross product of approximate tangent vectors.
+	const std::vector<Eigen::Vector3d>& getNormals() const;
 
-                const double xu = 1;
-                const double xv = 0;
-                const double yu = 0;
-                const double yv = 1;
+    const std::vector<Eigen::Vector3d>& getGlobalPoints() const;
 
-                normalsTmp[idx] = Eigen::Vector3d(yu*dv - yv*du, xv*du - xu*dv, xu*yv - yu*xv);
-				normalsTmp[idx].normalize();
-			}
-		}
+    const Sophus::SE3d& getGlobalPose() const;
 
-		// We set invalid normals for border regions.
-		for (int u = 0; u < width; ++u) {
-			normalsTmp[u] = Eigen::Vector3d(MINF, MINF, MINF);
-			normalsTmp[u + (height - 1) * width] = Eigen::Vector3d(MINF, MINF, MINF);
-		}
-		for (int v = 0; v < height; ++v) {
-			normalsTmp[v * width] = Eigen::Vector3d(MINF, MINF, MINF);
-			normalsTmp[(width - 1) + v * width] = Eigen::Vector3d(MINF, MINF, MINF);
-		}
+    const std::vector<double>& getDepthMap() const;
 
-		// We filter out measurements where either point or normal is invalid.
-		const unsigned nPoints = pointsTmp.size();
-		m_points.reserve(std::floor(double(nPoints) / downsampleFactor));
-		m_normals.reserve(std::floor(double(nPoints) / downsampleFactor));
+    const Eigen::Matrix3d& getIntrinsics() const;
 
-		for (int i = 0; i < nPoints; i = i + downsampleFactor) {
-			const auto& point = pointsTmp[i];
-			const auto& normal = normalsTmp[i];
+    const unsigned int getWidth() const;
 
-			if (point.allFinite() && normal.allFinite()) {
-				m_points.push_back(point);
-				m_normals.push_back(normal);
-			}
-		}
-	}
-
-	bool readFromFile(const std::string& filename) {
-		std::ifstream is(filename, std::ios::in | std::ios::binary);
-		if (!is.is_open()) {
-			std::cout << "ERROR: unable to read input file!" << std::endl;
-			return false;
-		}
-
-		char nBytes;
-		is.read(&nBytes, sizeof(char));
-
-		unsigned int n;
-		is.read((char*)&n, sizeof(unsigned int));
-
-		if (nBytes == sizeof(float)) {
-			float* ps = new float[3 * n];
-
-			is.read((char*)ps, 3 * sizeof(float) * n);
-
-			for (unsigned int i = 0; i < n; i++) {
-				Eigen::Vector3d p(ps[3 * i + 0], ps[3 * i + 1], ps[3 * i + 2]);
-				m_points.push_back(p);
-			}
-
-			is.read((char*)ps, 3 * sizeof(float) * n);
-			for (unsigned int i = 0; i < n; i++) {
-				Eigen::Vector3d p(ps[3 * i + 0], ps[3 * i + 1], ps[3 * i + 2]);
-				m_normals.push_back(p);
-			}
-
-			delete ps;
-		}
-		else {
-			double* ps = new double[3 * n];
-
-			is.read((char*)ps, 3 * sizeof(double) * n);
-
-			for (unsigned int i = 0; i < n; i++) {
-				Eigen::Vector3d p((float)ps[3 * i + 0], (float)ps[3 * i + 1], (float)ps[3 * i + 2]);
-				m_points.push_back(p);
-			}
-
-			is.read((char*)ps, 3 * sizeof(double) * n);
-
-			for (unsigned int i = 0; i < n; i++) {
-				Eigen::Vector3d p((float)ps[3 * i + 0], (float)ps[3 * i + 1], (float)ps[3 * i + 2]);
-				m_normals.push_back(p);
-			}
-
-			delete ps;
-		}
-
-		return true;
-	}
-
-	std::vector<Eigen::Vector3d>& getPoints() {
-		return m_points;
-	}
-
-	const std::vector<Eigen::Vector3d>& getPoints() const {
-		return m_points;
-	}
-
-	std::vector<Eigen::Vector3d>& getNormals() {
-		return m_normals;
-	}
-
-	const std::vector<Eigen::Vector3d>& getNormals() const {
-		return m_normals;
-	}
-
-    std::vector<Eigen::Vector3d>& getGlobalPoints() {
-        return m_points_global;
-    }
-
-    const Sophus::SE3d& getGlobalPose() const{
-        return global_pose;
-    }
-
-    const std::vector<double>& getDepthMap() const{
-        return m_depth_map;
-    }
-
-    const Eigen::Matrix3d& getIntrinsics() const{
-        return intrinsic_matrix;
-    }
-
-    const unsigned int getWidth() const{
-        return width;
-    }
-
-    const unsigned int getHeight() const{
-        return height;
-    }
+    const unsigned int getHeight() const;
 
 private:
+
+    std::vector<Eigen::Vector3d> computeCameraCoordinates(std::vector<double> depthMap,
+            unsigned int width, unsigned int height, Eigen::Matrix3d intrinsics);
+
+    std::vector<Eigen::Vector3d> computeNormals(std::vector<double> depthMap, unsigned int width, unsigned int height, double maxDistance = 0.1);
+
+    void addValidPoints(std::vector<Eigen::Vector3d> points, std::vector<Eigen::Vector3d> normals, int downsampleFactor);
+
 	std::vector<Eigen::Vector3d> m_points;
 	std::vector<Eigen::Vector3d> m_normals;
     std::vector<double> m_depth_map;
     std::vector<Eigen::Vector3d> m_points_global;
     Sophus::SE3d global_pose;
-    Eigen::Matrix3d intrinsic_matrix;
+    Eigen::Matrix3d m_intrinsic_matrix;
     size_t width;
     size_t height;
 
+    const unsigned int m_width;
+    const unsigned int m_height;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,9 @@
 #include <zconf.h>
 
 #include "DepthSensor.h"
+#include "icp.h"
 
-bool writeToFile(std::string filename, int width, int height, std::vector<float> vector){
+bool writeToFile(std::string filename, int width, int height, std::vector<double> vector){
     std::ofstream outFile(filename);
     if (!outFile.is_open()) return false;
 
@@ -22,14 +23,54 @@ bool writeToFile(std::string filename, int width, int height, std::vector<float>
 }
 
 int main(){
+    double dist_threshold = 0.001;
+    double normal_threshold = 0.001;
 
     DepthSensor sensor;
-
     sensor.start();
+
+    const Eigen::Matrix3d depthIntrinsics = sensor.GetIntrinsics();
+    const unsigned int depthWidth         = sensor.GetDepthImageWidth();
+    const unsigned int depthHeight        = sensor.GetDepthImageHeight();
+
+    sensor.ProcessNextFrame();
+    std::vector<double> depthMap = sensor.GetDepth();
+
+
+    // Frame(std::vector<double> depthMap, const Eigen::Matrix3d& depthIntrinsics, const unsigned int width, const unsigned int height) {
+
+    // Frame frame (depthMap, depthIntrinsics);
+
+
+
+    // create icp
+
+    // 0. create 1st frame
+
+        // 1. get depth map
+
+        // 2. create Frame (without extrinsics)
+
+        // 3. feed previous frame and created frame to icp
+        // frame.setExtrinsics(Sophus::SE3d)
+            // frame.applyGlobalTransform()
+
+        // icp.estimatePose(prev, current)
+
+        // frame.writeToMesh()
+
+    // 5. visual in a mesh.off
+
+
+
+
+    icp m_icp();
+
+    //sensor.start();
     sensor.ProcessNextFrame();
 
     std::cout << "Intrinsics" << std::endl;
-    std::cout << std::endl << sensor.getIntrinsics();
+    std::cout << std::endl << sensor.GetIntrinsics();
     int width = sensor.GetDepthImageWidth();
     std::cout << std::endl << "WIDTH: " << width;
     int height = sensor.GetDepthImageHeight();
@@ -42,6 +83,8 @@ int main(){
         usleep(5000000);
         std::cout << "capturing" << std::endl;
         sensor.ProcessNextFrame();
+
+        //std::cout << frame.
         auto map = sensor.GetDepth();
         writeToFile("depth.txt", width, height, map);
     }
