492015129921cd2fbf7c5d0180a34c16c1824770
Philipp Bock <philipp.bock@hotmail.de>
2019-07-11 14:10:54 +0200

    implemented Raycast with dummy functions

--- FusionLib/include/Raycast.hpp
+++ FusionLib/include/Raycast.hpp
@@ -15,7 +15,48 @@ public:
 
 private:
 
+    double interpolateNormals(const Eigen::Vector3d ,std::shared_ptr<Volume> volume );
 
+    /*!
+     *
+     * @param rayLength  pass-by reference, the variable that should later hold the rayLength
+     * @param volumeRange  the volumeSize scaled by the voxelscale, this "equals" the biggest possible distance of the ray
+     * @param origin    the origin off the camera equals the translation
+     * @param direction  the direction equals the rotation * pixel position
+     * @return
+     */
+    bool calculateRayLength(double& rayLength, const Eigen::Vector3d volumeRange,const Eigen::Vector3d& origin, const Eigen::Vector3d& direction);
+    /*!
+     *
+     * @param x
+     * @param y
+     * @param rotation
+     * @param intrinsics
+     * @return the normalized direction of the ray which equals the rotation*cameraSpaceCoordinates
+     */
+    const Eigen::Vector3d calculateRayDirection(int x,int y,Eigen::Matrix<double, 3, 3, Eigen::DontAlign> rotation, Eigen::Matrix3d intrinsics);
+
+    /*!
+     * This method should not only calculate the currentPoint on the ray but also check wether it is inside the volume 1<=x<=volume.Size.x()-1,...
+     * in reference implementation this equals the calculation of the variable grid
+     *
+     * @param currentPoint pass-by-reference of the point to be calculated
+     * @param rayParameter e.g. rayLength
+     * @param volumeSize  here volumeSize can be used but currentPoint must be divided by voxelScale
+     * @param origin  translation
+     * @param direction rotation*currentPoint
+     * @return is point in Volume
+     */
+    bool calculateCurrentPointOnRay(Eigen::Vector3d& currentPoint, double& rayParameter, const Eigen::Vector3d volumeSize,const Eigen::Vector3d& origin, const Eigen::Vector3d& direction);
+
+    /*!
+     * Attention/TODO: make sure the correct tsdf is retrieved as the mapping is dependant on the for loops in fusion
+     *
+     * @param volume
+     * @param position
+     * @return the tsdf from the map in Volume at position, check the way it is done in Fusion
+     */
+    double getTSDF(std::shared_ptr<Volume> volume, Eigen::Vector3d position);
 };
 
 
--- FusionLib/src/Raycast.cpp
+++ FusionLib/src/Raycast.cpp
@@ -8,5 +8,100 @@ bool Raycast::surfacePrediction(std::shared_ptr<Frame> currentFrame,std::shared_
 
     //TODO: implement raycasting
 
+    auto volumeSize =volume->getVolumeSize();
+    auto voxelScale = volume->getVoxelScale();
+    auto pose = currentFrame->getGlobalPose();
+    auto width = currentFrame->getWidth();
+    auto height = currentFrame->getHeight();
+
+    const Eigen::Vector3d volumeRange(volumeSize.x()*voxelScale,volumeSize.y()*voxelScale,volumeSize.z()*voxelScale);
+
+        for( int y =0;y<height;y++){
+            for(int x=0;x< width;x++){
+
+                //calculate Normalized Direction
+                auto direction = calculateRayDirection(x,y,pose.rotationMatrix(),currentFrame->getIntrinsics());
+
+                //calculate rayLength
+                double rayLength(0);
+                if(!calculateRayLength(rayLength,volumeRange,pose.translation(),direction))continue;
+
+                Eigen::Vector3d currentPoint;
+                calculateCurrentPointOnRay(currentPoint,rayLength,volumeSize,pose.translation(),direction);
+
+                double TSDF = getTSDF(volume,currentPoint);
+
+                const double maxSearchLength = rayLength + volumeRange.x() * sqrt(2.f);
+                //why truncationDistance*0.5?
+
+                for(rayLength;rayLength<maxSearchLength;rayLength+=truncationDistance*0.5f){
+
+                    if(!calculateCurrentPointOnRay(currentPoint,rayLength,volumeSize,pose.translation(),direction))continue;
+                    const double previousTSDF = TSDF;
+                    getTSDF(volume,currentPoint);
+
+                    //This equals -ve to +ve in the paper / we cant go from a negative to positive tsdf value as negative is behind the surface
+                    if(previousTSDF<0. && TSDF>0.)break;
+                    //this equals +ve to -ve in the paper / this means we just crossed a zero value
+                    if(previousTSDF>0. && TSDF<0.)
+                        //We reached the zero crossing
+
+                        //TODO: Calculate vertex
+
+                        //TODO: Calculated normal using interpolation method
+
+                        //TODO: set global vertex & normal into currentFrame
+
+
+
+                    }
+
+
+
+                }
+
+
+
+
+
+            }
+        }
+
+
+
     return true;
+}
+
+double Raycast::interpolateNormals(const Eigen::Vector3d, std::shared_ptr<Volume> volume) {
+     // TODO implement, check Method signature
+    return 0;
+}
+
+bool Raycast::calculateRayLength(double &rayLength, const Eigen::Vector3d volumeRange, const Eigen::Vector3d &origin,
+                                 const Eigen::Vector3d &direction) {
+    // TODO implement, check Method signature
+
+    return false;
+}
+
+const Eigen::Vector3d
+Raycast::calculateRayDirection(int x, int y, Eigen::Matrix<double, 3, 3, Eigen::DontAlign> rotation,
+                               Eigen::Matrix3d intrinsics) {
+    // TODO implement, check Method signature
+
+    return Eigen::Vector3d();
+}
+
+bool Raycast::calculateCurrentPointOnRay(Eigen::Vector3d &currentPoint, double &rayParameter,
+                                         const Eigen::Vector3d volumeSize, const Eigen::Vector3d &origin,
+                                         const Eigen::Vector3d &direction) {
+    // TODO implement, check Method signature
+
+    return false;
+}
+
+double Raycast::getTSDF(std::shared_ptr<Volume> volume, Eigen::Vector3d position) {
+    // TODO implement, check Method signature
+
+    return 0;
 }
