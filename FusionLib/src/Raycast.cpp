5d09be75657f5b67ec86a5f86ea135764a6d9a93
rinkita <mittal.rinkita@gmail.com>
2019-07-14 08:05:47 +0200

    implementation for some functions

--- FusionLib/src/Raycast.cpp
+++ FusionLib/src/Raycast.cpp
@@ -77,19 +77,45 @@ double Raycast::interpolateNormals(const Eigen::Vector3d, std::shared_ptr<Volume
     return 0;
 }
 
+double get_max_time(const Eigen::Vector3d volumeRange, const Eigen::Vector3d &origin, const Eigen::Vector3d &direction)
+{
+    double txmax = ((direction.x() > 0 ? volumeRange.x() : 0.f) - origin.x()) / direction.x();
+    double tymax = ((direction.y() > 0 ? volumeRange.y(): 0.f) - origin.y()) / direction.y();
+    double tzmax = ((direction.z() > 0 ? volumeRange.z() : 0.f) - origin.z()) / direction.z();
+
+    return min(min(txmax, tymax), tzmax);
+}
+
 bool Raycast::calculateRayLength(double &rayLength, const Eigen::Vector3d volumeRange, const Eigen::Vector3d &origin,
                                  const Eigen::Vector3d &direction) {
     // TODO implement, check Method signature
+    float txmin = ((direction.x() > 0 ? 0.f : volumeRange.x()) - origin.x()) / direction.x();
+    float tymin = ((direction.y() > 0 ? 0.f : volumeRange.y()) - origin.y()) / direction.y();
+    float tzmin = ((direction.z() > 0 ? 0.f : volumeRange.z()) - origin.z()) / direction.z();
 
-    return false;
+    rayLength =  max(max(txmin, tymin), tzmin);
+    if(rayLength >= get_max_time(volumeRange, origin, direction))
+    {
+        return false;
+    }
+    return  true;
+
 }
 
 const Eigen::Vector3d
 Raycast::calculateRayDirection(int x, int y, Eigen::Matrix<double, 3, 3, Eigen::DontAlign> rotation,
                                Eigen::Matrix3d intrinsics) {
     // TODO implement, check Method signature
+    double fovX = intrinsics(0, 0);
+    double fovY = intrinsics(1, 1);
+    double cX = m_intrinsics(0, 2);
+    double cY = m_intrinsics(1, 2);
 
-    return Eigen::Vector3d();
+    Eigen::Vector3d cameraPoint = Eigen::Vector3d((x - cX) / fovX , (y - cY) / fovY ,1.d);
+    Eigen::Vector3d rayDirection = rotation * cameraPoint;
+    rayDirection.normalize();
+
+    return rayDirection;
 }
 
 bool Raycast::calculateCurrentPointOnRay(Eigen::Vector3d &currentPoint, double &rayParameter,
@@ -102,6 +128,8 @@ bool Raycast::calculateCurrentPointOnRay(Eigen::Vector3d &currentPoint, double &
 
 double Raycast::getTSDF(std::shared_ptr<Volume> volume, Eigen::Vector3d position) {
     // TODO implement, check Method signature
-
-    return 0;
+    std::pair<double,double> fusionPoints= volume->getPoints()[position.x() + position.y()*volumeSize.x()
+                                                               + position.z()*volumeSize.x()*volumeSize.y()];
+    const double tsdf = fusionPoints.first;
+    return tsdf;
 }
