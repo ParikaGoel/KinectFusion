2ab9f7ff5feb5f3276934a3fb9192a951cd15725
Frank <frankz.lu@gmail.com>
2019-07-16 17:38:10 +0200

    working main

--- main.cpp
+++ main.cpp
@@ -11,10 +11,14 @@
 #include <Volume.hpp>
 #include <Fusion.hpp>
 #include <Raycast.hpp>
+#include <Recorder.h>
+#include <MeshWriter.h>
+#include <KinectVirtualSensor.h>
 
 #include "VirtualSensor.h"
 #include "icp.h"
 #include "Frame.h"
+
 
 //TODO this should be moved to one File containing all data_declarations class
 struct Config{
@@ -44,7 +48,7 @@ bool process_frame( size_t frame_cnt, std::shared_ptr<Frame> prevFrame,std::shar
     Eigen::Matrix4d estimated_pose = prevFrame->getGlobalPose();
     currentFrame->setGlobalPose(estimated_pose);
 
-    if(!icp.estimatePose(frame_cnt, prevFrame,currentFrame, 10, estimated_pose)){
+    if(!icp.estimatePose(frame_cnt, prevFrame,currentFrame, 3, estimated_pose)){
         throw "ICP Pose Estimation failed";
     };
 
@@ -60,22 +64,33 @@ bool process_frame( size_t frame_cnt, std::shared_ptr<Frame> prevFrame,std::shar
     if(!raycast.surfacePrediction(currentFrame,volume, config.m_truncationDistance)){
         throw "Raycasting failed";
     };
-
     return true;
 }
 
 int main(){
-    // 5. visual in a mesh.off
-    std::string filenameIn = PROJECT_DATA_DIR + std::string("/rgbd_dataset_freiburg1_xyz/");
-    std::string filenameBaseOut = PROJECT_DATA_DIR + std::string("/results/mesh_");
 
-    // Load video
-    std::cout << "Initialize virtual sensor..." << std::endl;
     VirtualSensor sensor;
+    std::string filenameIn = PROJECT_DATA_DIR + std::string("/rgbd_dataset_freiburg1_xyz/");
+
+    //std::string filenameIn = PROJECT_DATA_DIR + std::string("/rs9/");
+
     if (!sensor.init(filenameIn)) {
         std::cout << "Failed to initialize the sensor!\nCheck file path!" << std::endl;
         return -1;
     }
+
+    //Recorder rec;
+    // rec.record(20);
+
+    // 5. visual in a mesh.off
+    //std::string filenameIn = PROJECT_DATA_DIR + std::string("/rgbd_dataset_freiburg1_xyz/");
+    std::string filenameBaseOut = PROJECT_DATA_DIR + std::string("/results/mesh_");
+
+    // Load video
+    std::cout << "Initialize virtual sensor..." << std::endl;
+    // VirtualSensor sensor;
+    // KinectVirtualSensor sensor(PROJECT_DATA_DIR + std::string("/sample0"), 5 );
+
 
     // We store a first frame as a reference frame. All next frames are tracked relatively to the first frame.
     sensor.processNextFrame();
@@ -86,38 +101,52 @@ int main(){
     //Setup Volume
     auto volume = std::make_shared<Volume>(config.m_volumeSize,config.m_voxelScale) ;
 
-    const Eigen::Matrix3d depthIntrinsics = sensor.getDepthIntrinsics();
+    Eigen::Matrix3d depthIntrinsics1 = sensor.getDepthIntrinsics();
+    // depthIntrinsics1(0,2) = 0;
+    // depthIntrinsics1(1,2) = 0;
+    const Eigen::Matrix3d depthIntrinsics = depthIntrinsics1;
     const unsigned int depthWidth         = sensor.getDepthImageWidth();
     const unsigned int depthHeight        = sensor.getDepthImageHeight();
 
-    std::shared_ptr<Frame> prevFrame = std::make_shared<Frame>(Frame(sensor.getDepth(), depthIntrinsics, depthWidth, depthHeight));
+    const double* depthMap = &sensor.getDepth()[0];
+    BYTE* colors = &sensor.getColorRGBX()[0];
+
+    std::shared_ptr<Frame> prevFrame = std::make_shared<Frame>(Frame(depthMap, depthIntrinsics, depthWidth, depthHeight));
+
+    std::cout << std::endl;
+    auto map = prevFrame->getDepthMap();
+
+    std::vector<Eigen::Vector3d> test(depthWidth*depthHeight);
+    for (size_t x = 0; x < depthWidth*depthHeight; x++) {
+            test[x] = Eigen::Vector3d(x%depthWidth, int (x/depthWidth), map[x]*1000);
+    }
+
+    MeshWriter::toFile("test", colors, test);
+
+    MeshWriter::toFile( "my_mesh", colors, prevFrame->getPoints());
 
     Eigen::Matrix4d init_gl_pose = Eigen::Matrix4d::Identity();
 
     Eigen::Matrix4d current_camera_to_world = init_gl_pose;
 
-    auto normals   = prevFrame->getNormals();
-    auto g_normals = prevFrame->getGlobalNormals();
-    for (size_t i = 0; i < normals.size(); i++){
-        if((normals[i] - g_normals[i]).norm() > 0.001)
-        {
-            std::cout << "err" << normals[i] << std::endl;
-        }
-    }
-
     int i = 0;
-    const int iMax = 52;
+    const int iMax = 45;
 
     std::stringstream ss;
     ss << filenameBaseOut << i << ".off";
-    prevFrame->WriteMesh(ss.str(), "0 0 255 255");
+    prevFrame->WriteMesh(ss.str(), "0 0 255 100");
 
     while(sensor.processNextFrame() && i <= iMax){
 
-        std::shared_ptr<Frame> currentFrame = std::make_shared<Frame>(Frame(sensor.getDepth(),depthIntrinsics, depthWidth, depthHeight));
+        const double* depthMap = &sensor.getDepth()[0];
+        std::shared_ptr<Frame> currentFrame = std::make_shared<Frame>(Frame(depthMap,depthIntrinsics, depthWidth, depthHeight));
         process_frame(i,prevFrame,currentFrame,volume,config);
 
+        std::stringstream ss;
         ss << filenameBaseOut << i << ".off";
+
+        std::cout << "Writing Mesh " << i << std::endl;
+
         currentFrame->WriteMesh(ss.str(), "255 0 0 255");
 
         prevFrame = std::move(currentFrame);
