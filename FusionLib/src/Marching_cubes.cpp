cd84a61cb091e04623b02aeac4f2aea4d2c52aac
ga96tul <Freemander10>
2019-07-20 20:40:18 +0200

    closest point marching cubes

--- FusionLib/src/Marching_cubes.cpp
+++ FusionLib/src/Marching_cubes.cpp
@@ -21,12 +21,12 @@ struct triangleShape {
 };
 
 Eigen::Vector3d interpolate(VoxelWCoords v1, VoxelWCoords v2) {
-	double t = (0.5 - v1._data.tsdf) / (v2._data.tsdf - v1._data.tsdf);
+	double t = (0 - v1._data.tsdf) / (v2._data.tsdf - v1._data.tsdf);
 	Eigen::Vector3d vec1, vec2;
 	vec1 << v1._x, v1._y, v1._z;
 	vec2 << v2._x, v2._y, v2._z;
 //	if(t!=0.5)std::cout<<"t:"<<t<<"tsdf1: "<<v1._data.tsdf<<"tsdf2: "<<v2._data.tsdf<<std::endl;
-	return vec1 + 0.5 * (vec2 - vec1);
+	return vec1 + t * (vec2 - vec1);
 }
 
 const int calculateCoordinateToIndex(int x, int y, int z, Eigen::Vector3i volumeSize) {
@@ -40,8 +40,7 @@ std::string printTriangleVertices(triangleShape tri, double voxelScale) {
 //																												""<<std::endl;
 
 	color << static_cast<unsigned>(tri.color.x()) << " " << static_cast<unsigned>(tri.color.y()) << " " << static_cast<unsigned>(tri
-	.color.z()) << " "
-	<< std::endl;
+	.color.z());
 	std::stringstream ss;
 
 	ss << tri._idx1.x() << " " << tri._idx1.y() << " " << tri._idx1.z() << " " << color.str() << std::endl;
@@ -91,54 +90,114 @@ void MarchingCubes::extractMesh(Volume &volume, std::string fileName) {
 				double isoLevel = 0.0;
 				Vector4uc averageColor;
 				int contributors = 0;
-				if (points[0]._data.tsdf < isoLevel) {
+				bool valid = true;
+				for(int i = 0;i<8;i++){
+					if(points[i]._data.weight  ==0.)valid = false;
+				}
+				if(!valid) continue;
+				double tsdf =-10;
+//				double tsdf =-10;
+
+				if (points[0]._data.tsdf <= isoLevel && points[0]._data.weight != 0) {
 					cubeIndex |= 1;
-					/*std::cout << static_cast<unsigned>(points[0]._data.color.x()) << " "
-							  << static_cast<unsigned>(points[0]._data.color.y()) << " "
-																					 ""
-							  << static_cast<unsigned>(points[0]._data.color.z()) << " "
-																					 "" << std::endl;*/
-					averageColor += points[0]._data.color;
-					contributors++;
+					if(points[0]._data.color.x()<20 && points[0]._data.color.y()<20 && points[0]._data.color.z()<20 ){
+//						std::cout<<points[0]._data.tsdf<<std::endl;
+					}
+					if(points[0]._data.tsdf>tsdf){
+						averageColor = points[0]._data.color;
+						contributors++;
+						tsdf = points[0]._data.tsdf;
+
+					}
+
+
 				}
-				if (points[1]._data.tsdf < isoLevel && points[1]._data.weight != 0) {
-					averageColor += points[1]._data.color;
-					contributors++;
+				if (points[1]._data.tsdf <= isoLevel && points[1]._data.weight != 0) {
+					if(points[1]._data.color.x()<20 && points[1]._data.color.y()<20 && points[1]._data.color.z()<20 ){
+//						std::cout<<points[1]._data.tsdf<<std::endl;
+					}
 					cubeIndex |= 2;
+					if(points[1]._data.tsdf>tsdf){
+						averageColor = points[1]._data.color;
+						contributors++;
+						tsdf = points[1]._data.tsdf;
+
+					}
 				}
-				if (points[2]._data.tsdf < isoLevel && points[2]._data.weight != 0) {
-					averageColor += points[2]._data.color;
-					contributors++;
+				if (points[2]._data.tsdf <= isoLevel && points[2]._data.weight != 0) {
+					if(points[2]._data.color.x()<20 && points[2]._data.color.y()<20 && points[2]._data.color.z()<20 ){
+//						std::cout<<points[2]._data.tsdf<<std::endl;
+					}
 					cubeIndex |= 4;
+					if(points[2]._data.tsdf>tsdf){
+						averageColor = points[2]._data.color;
+						contributors++;
+						tsdf = points[2]._data.tsdf;
+
+					}
 				}
-				if (points[3]._data.tsdf < isoLevel && points[3]._data.weight != 0) {
-					averageColor += points[3]._data.color;
-					contributors++;
+				if (points[3]._data.tsdf <= isoLevel && points[3]._data.weight != 0) {
+					if(points[3]._data.color.x()<20 && points[3]._data.color.y()<20 && points[3]._data.color.z()<20 ){
+//						std::cout<<points[3]._data.tsdf<<std::endl;
+					}
 					cubeIndex |= 8;
+					if(points[3]._data.tsdf>tsdf){
+						averageColor = points[3]._data.color;
+						contributors++;
+						tsdf = points[3]._data.tsdf;
+
+					}
 				}
-				if (points[4]._data.tsdf < isoLevel && points[4]._data.weight != 0) {
-					averageColor += points[4]._data.color;
-					contributors++;
+				if (points[4]._data.tsdf <= isoLevel && points[4]._data.weight != 0) {
+					if(points[4]._data.color.x()<20 && points[4]._data.color.y()<20 && points[4]._data.color.z()<20 ){
+//						std::cout<<points[4]._data.tsdf<<std::endl;
+					}
 					cubeIndex |= 16;
+					if(points[4]._data.tsdf>tsdf){
+						averageColor = points[4]._data.color;
+						contributors++;
+						tsdf = points[4]._data.tsdf;
+
+					}
 				}
-				if (points[5]._data.tsdf < isoLevel && points[5]._data.weight != 0) {
-					averageColor += points[5]._data.color;
-					contributors++;
+				if (points[5]._data.tsdf <= isoLevel && points[5]._data.weight != 0) {
+					if(points[5]._data.color.x()<20 && points[5]._data.color.y()<20 && points[5]._data.color.z()<20 ){
+//						std::cout<<points[5]._data.tsdf<<std::endl;
+					}
 					cubeIndex |= 32;
+					if(points[5]._data.tsdf>tsdf){
+						averageColor = points[5]._data.color;
+						contributors++;
+						tsdf = points[5]._data.tsdf;
+
+					}
 				}
-				if (points[6]._data.tsdf < isoLevel && points[6]._data.weight != 0) {
-					averageColor += points[6]._data.color;
-					contributors++;
+				if (points[6]._data.tsdf <= isoLevel && points[6]._data.weight != 0) {
+					if(points[6]._data.color.x()<20 && points[6]._data.color.y()<20 && points[6]._data.color.z()<20 ){
+//						std::cout<<points[6]._data.tsdf<<std::endl;
+					}
 					cubeIndex |= 64;
+					if(points[6]._data.tsdf>tsdf){
+						averageColor = points[6]._data.color;
+						contributors++;
+						tsdf = points[6]._data.tsdf;
+
+					}
 				}
-				if (points[7]._data.tsdf < isoLevel && points[7]._data.weight != 0) {
-					averageColor += points[7]._data.color;
-					contributors++;
+				if (points[7]._data.tsdf <= isoLevel && points[7]._data.weight != 0) {
+					if(points[7]._data.color.x()<20 && points[7]._data.color.y()<20 && points[7]._data.color.z()<20 ){
+//						std::cout<<points[7]._data.tsdf<<std::endl;
+					}
 					cubeIndex |= 128;
+					if(points[7]._data.tsdf>tsdf){
+						averageColor = points[7]._data.color;
+						contributors++;
+						tsdf = points[7]._data.tsdf;
+					}
 				}
-				if (contributors != 0) {
+				/*if (contributors != 0) {
 					averageColor /= contributors;
-				}
+				}*/
 
 				//create triangles for printing out
 				// Create triangles for current cube configuration
