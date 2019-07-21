2b0e2e9afe27e9d509fcbee564c6e9fe6005742f
Frank <frankz.lu@gmail.com>
2019-07-21 18:42:17 +0200

    recorder realsense fix

--- FusionLib/include/Recorder.h
+++ FusionLib/include/Recorder.h
@@ -23,14 +23,14 @@ public:
             auto color_frame = sensor.getFrameset().get_color_frame();
 
             std::stringstream png_file;
-            png_file << PROJECT_DATA_DIR << "/rs-depth/" << i << ".png";
+            png_file << PROJECT_DATA_DIR << "/recording/rs-depth/" << i << ".png";
             stbi_write_png(png_file.str().c_str(), frame.get_width(), frame.get_height(),
                            frame.get_bytes_per_pixel(), frame.get_data(), frame.get_stride_in_bytes());
 
             depth_info << i << " " << "rs-depth/" << i << ".png" << std::endl;
 
             std::stringstream png_file2;
-            png_file2 << PROJECT_DATA_DIR << "/rs-rgb/" << i << ".png";
+            png_file2 << PROJECT_DATA_DIR << "/recording/rs-rgb/" << i << ".png";
             stbi_write_png(png_file2.str().c_str(), color_frame.get_width(), color_frame.get_height(),
                            color_frame.get_bytes_per_pixel(), color_frame.get_data(), color_frame.get_stride_in_bytes());
 
@@ -40,7 +40,7 @@ public:
         }
 
         std::stringstream dir;
-        dir << PROJECT_DATA_DIR;
+        dir << PROJECT_DATA_DIR << "/recording";
 
         std::string intro = "# images\n# file\n # timestamp filename\n";
         std::ofstream out (dir.str()+ "/depth.txt");
